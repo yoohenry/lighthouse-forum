e5359b9413ce81eb83e60c944a88f0d0b14d91f9
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-27 18:47:01 +0000

    adds edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts',     to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
-  post 'posts',    to: 'posts#create'
-
+  get 'posts',          to: 'posts#index'
+  get 'posts/new',      to: 'posts#new'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id',      to: 'posts#show', as: 'post' # necessary for the update action!
+  patch 'posts/:id',    to: 'posts#update'
+  post 'posts',         to: 'posts#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
