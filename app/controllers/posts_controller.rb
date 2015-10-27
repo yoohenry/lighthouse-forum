d03637125b63393e48856165ae736b4686df7a0c
Henry Yoo <henryhs.yoo@gmail.com>
2015-10-27 18:33:55 +0000

    adds new posts functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,31 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title:  params[:post][:title],
+      author: params[:post][:author],
+      text:   params[:post][:text]
+    )
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+
+  end
+
+  protected
+
+  def post_params
+    params.require(:post).permit(:title, :author, :text)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 LighthouseForum::Application.routes.draw do
 
-  get 'posts', to: 'posts#index'
+  get 'posts',     to: 'posts#index'
+  get 'posts/new', to: 'posts#new'
   get 'posts/:id', to: 'posts#show'
+  post 'posts',    to: 'posts#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
